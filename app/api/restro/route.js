7fc9dbd6dc4598b8fe371f975c4c6f877fd6e65e
SaiRamyaKamali <srkamali1326@gmail.com>
2023-05-04 17:47:29 -0500

    backend

---
+++ app/api/restro/route.js
@@ -0,0 +1,10 @@
+export async function POST(request) {
+
+  const {preferences} = request.body;
+
+  const apiKey = 'AIzaSyBDx0Jt2uc5577zBvhflHCmnAS-fe_y_3s';
+  const apiUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522%2C151.1957362&radius=1500&type=restaurant&keyword=${preferences.cuisine}&key=${apiKey}`;
+
+  const response = await fetch(apiUrl);
+  return new Response({response.data})
+}
