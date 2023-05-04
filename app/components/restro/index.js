298e86094a6cc0e27016acf3c2047243eeb70b61
jaranzie <>
2023-05-04 17:46:38 -0400

    backend handler

--- app/components/restro/index.js
+++ app/components/restro/index.js
@@ -8,9 +8,15 @@ const Selection = ({ username, preferences}) => {
   useEffect(() => {
     async function getNearbyRestaurants() {
       try {
-        const response = await fetch(apiUrl);
+        const response = await fetch(`/api/restro`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ preferences }),
+        });
         const data = await response.json();
-        setRestaurants(data.results);
+        setRestaurants(data);
       } catch (error) {
         console.error(error);
       }
@@ -34,3 +40,40 @@ const Selection = ({ username, preferences}) => {
 };
 
 export default Selection;
+
+// import React, { useEffect, useState } from 'react';
+
+// const Selection = ({ username, preferences}) => {
+//   const [restaurants, setRestaurants] = useState([]);
+//   const apiKey = 'AIzaSyBDx0Jt2uc5577zBvhflHCmnAS-fe_y_3s';
+//   const apiUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522%2C151.1957362&radius=1500&type=restaurant&keyword=${preferences.cuisine}&key=${apiKey}`;
+
+//   useEffect(() => {
+//     async function getNearbyRestaurants() {
+//       try {
+//         const response = await fetch(apiUrl);
+//         const data = await response.json();
+//         setRestaurants(data.results);
+//       } catch (error) {
+//         console.error(error);
+//       }
+//     }
+
+//     if (preferences.location && preferences.cuisine && preferences.price) {
+//       getNearbyRestaurants();
+//     }
+//   }, [preferences]);
+
+//   return (
+//     <div>
+//       <h2>{username}'s Selection:</h2>
+//       <ul>
+//         {restaurants.map((restaurant) => (
+//           <li key={restaurant.place_id}>{restaurant.name}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default Selection;
