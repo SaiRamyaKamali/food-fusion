1907207cfa448d1fa889b55f8205092558108292
SaiRamyaKamali <srkamali1326@gmail.com>
2023-05-04 20:51:59 -0500

    fixed location

--- app/api/restro/route.js
+++ app/api/restro/route.js
@@ -3,12 +3,18 @@ import { NextResponse } from "next/server";
 
 export async function POST(request) {
 
-  // const {preferences} = await request.body.JSON();
-
   const preferences = await request.json();
 
+  // const {preferences} = await request.body.JSON();
+  const locationApiKey = 'AIzaSyDpyTvBfWLYTRIR7BlWqt9fo_Px10IpAsU'
+  const geocodingApiUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${preferences.location}&key=${locationApiKey}`;
+  const geocodingResponse = await fetch(geocodingApiUrl);
+  const geocodingData = await geocodingResponse.json();
+  const location = geocodingData.results[0].geometry.location;
+
+
   const apiKey = 'AIzaSyBDx0Jt2uc5577zBvhflHCmnAS-fe_y_3s';
-  const apiUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522%2C151.1957362&radius=1500&type=restaurant&keyword=${preferences.cuisine}&key=${apiKey}`;
+  const apiUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location.lat},${location.lng}&radius=1500&type=restaurant&keyword=${preferences.cuisine}&key=${apiKey}`;
 
   const response = await fetch(apiUrl);
   const data = await response.json();
